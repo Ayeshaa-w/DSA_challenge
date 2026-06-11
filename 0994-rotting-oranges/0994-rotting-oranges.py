b0e4775e32eadecaff053946c964a240cfc5c3d2
AYESHA ZAFREEN S <ayeshaaazz12@gmail.com>
2026-06-11 16:12:20 +0530

    Time: 3 ms (76.49%), Space: 19.4 MB (41.32%) - LeetHub

--- 0994-rotting-oranges/0994-rotting-oranges.py
+++ 0994-rotting-oranges/0994-rotting-oranges.py
@@ -1,40 +1,29 @@
-from collections import deque
-from typing import List
-
 class Solution:
     def orangesRotting(self, grid: List[List[int]]) -> int:
-        rows, cols = len(grid), len(grid[0])
-        q = deque()
-
-        def addroom(r, c):
-            if (r < 0 or r == rows or c < 0 or c == cols or grid[r][c] != 1):
+        rows,cols=len(grid),len(grid[0])
+        q=deque()
+        def addroom(r,c):
+            if (r<0 or c<0 or r==rows or c==cols or grid[r][c]!=1):
                 return
-            grid[r][c] = 2   # 🔥 mark as rotten immediately
-            q.append((r, c))
-
-        # Start from all rotten oranges
-        for r in range(rows):
-            for c in range(cols):
-                if grid[r][c] == 2:
-                    q.append((r, c))
-
-        dist = 0
-
+            grid[r][c]=2
+            q.append([r,c])
+        for rs in range(rows):
+            for cs in range(cols):
+                if grid[rs][cs]==2:
+                    q.append([rs,cs])
+        dist =0
         while q:
             for _ in range(len(q)):
-                r, c = q.popleft()
-
-                addroom(r + 1, c)
-                addroom(r - 1, c)
-                addroom(r, c + 1)
-                addroom(r, c - 1)
-
-            dist += 1
-
-        # Check any fresh orange left
-        for r in range(rows):
-            for c in range(cols):
-                if grid[r][c] == 1:
+                r,c=q.popleft()
+                addroom(r+1,c)
+                addroom(r-1,c)
+                addroom(r,c+1)
+                addroom(r,c-1)
+            dist+=1
+        for R in range(rows):
+            for C in range(cols):
+                if grid[R][C]==1:
                     return -1
+        return dist-1 if dist>0 else 0
 
-        return dist - 1 if dist > 0 else 0
+        
