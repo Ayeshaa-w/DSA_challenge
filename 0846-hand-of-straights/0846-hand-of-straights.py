c98660254f2f8c6b8b7d32236dd5ec22b7ae72e7
AYESHA ZAFREEN S <ayeshaaazz12@gmail.com>
2026-07-28 11:27:41 +0530

    Time: 26 ms (89.51%), Space: 20.9 MB (95.08%) - LeetHub

--- 0846-hand-of-straights/0846-hand-of-straights.py
+++ 0846-hand-of-straights/0846-hand-of-straights.py
@@ -2,23 +2,15 @@ class Solution:
     def isNStraightHand(self, hand: List[int], groupSize: int) -> bool:
         if len(hand)%groupSize!=0:
             return False
-        n=len(hand)//groupSize #no of groups to be formed
-        hand=sorted(hand) #sort the ip
-        arr=[[] for _ in range(n)] #[[],[],[]]
-        for h in hand:
-            placed=False
-            for j in range(n):
-                if (len(arr[j])>0 and len(arr[j])<groupSize and arr[j][-1]+1==h):
-                    arr[j].append(h)
-                    placed=True
-                    break
-            if not placed:
-                for j in range(n):
-                    if len(arr[j])==0:
-                        arr[j].append(h)
-                        placed=True
-                        break
-            if not placed:
-                return False
+        count=Counter(hand)
+        hand=sorted(hand)
+        for num in hand:
+            if count[num]:
+                for i in range(num,num+groupSize):
+                    if not count[i]:
+                        return False
+                    count[i]-=1
         return True
+
+
         
