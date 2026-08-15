87e7866a020a64131978f8e44fd3cd3df66e4b50
AYESHA ZAFREEN S <ayeshaaazz12@gmail.com>
2026-08-15 06:39:52 +0530

    Time: 119 ms (33.81%), Space: 19.7 MB (37.16%) - LeetHub

--- 0076-minimum-window-substring/0076-minimum-window-substring.py
+++ 0076-minimum-window-substring/0076-minimum-window-substring.py
@@ -1,27 +1,26 @@
 class Solution:
     def minWindow(self, s: str, t: str) -> str:
-        count={}
         window={}
-        have,l=0,0
+        countt={}
+        have=0
         string=""
-        min_len=float('inf')
-        for i in range(len(t)):
-            count[t[i]]=1+count.get(t[i],0)
-        need=len(count)
+        reslen=float('inf')
+        for i in t:
+            countt[i]=countt.get(i,0)+1
+        need=len(countt)
+        l=0
         for r in range(len(s)):
             c=s[r]
-            window[c]=1+window.get(c,0)
-            if c in count and window[c]==count[c]:
+            window[s[r]]=window.get(c,0)+1
+            if c in countt and window[c]==countt[c]:
                 have+=1
-                print(f"have:{have}")
             while have==need:
-                if (r-l+1)<min_len:
-                    min_len=(r-l+1)
+                if (r-l+1)<reslen:
+                    reslen=(r-l+1)
                     string=s[l:r+1]
                 window[s[l]]-=1
-                if s[l] in count and window[s[l]]<count[s[l]]:
+                if s[l] in countt and window[s[l]] < countt[s[l]]:
                     have-=1
                 l+=1
-        return string 
-        
+        return string
         
